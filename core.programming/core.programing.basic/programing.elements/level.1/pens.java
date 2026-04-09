cf8b812653c6cacff20bf7251a57facaa39ae738
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-09 12:55:09 +0530

    added ques 5 in level 1

---
+++ core.programming/core.programing.basic/programing.elements/level.1/pens.java
@@ -0,0 +1,11 @@
+public class pens {
+    public static void main(String[] args) {
+        int pen = 14;
+        int student = 3;
+        int rem = pen % student;
+        int qua = pen / student;
+        System.out.println("The Pen Per Student is " + qua + " and the remaining pen not distributed is " +
+                rem);
+    }
+
+}
