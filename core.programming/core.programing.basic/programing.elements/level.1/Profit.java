bb6e33bc66433ba0bd79c7bb1fc7c2e596ec59a7
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-09 12:45:07 +0530

    added ques 4 in level 1 file

---
+++ core.programming/core.programing.basic/programing.elements/level.1/Profit.java
@@ -0,0 +1,10 @@
+public class Profit {
+    public static void main(String[] args) {
+        int cost_price = 129;
+        int seling_price = 191;
+        int profit = seling_price - cost_price;
+        int profit_percentge = (profit * 100) / cost_price;
+        System.out.print(" The profit is " + profit + " and the profit percentage is " + profit_percentge);
+    }
+
+}
