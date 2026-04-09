c5406aff7c617be146c07b96a7f040f77ee375b6
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-09 12:12:34 +0530

    Added Level-1 file

---
+++ core.programming/core.programing.basic/programing.elements/level.1/CurrentYear.java
@@ -0,0 +1,9 @@
+public class CurrentYear {
+    public static void main(String[] args) {
+        int harry_birth_year = 2000;
+        int current_year = 2024;
+        int harry_age = current_year - harry_birth_year;
+        System.out.println(harry_age);
+
+    }
+}
