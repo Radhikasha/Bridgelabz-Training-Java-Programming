9a2503bd3b81b7b77188eb05ac7333f500980838
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-09 12:36:25 +0530

    added ques 3 in level 1

---
+++ core.programming/core.programing.basic/programing.elements/level.1/Distance.java
@@ -0,0 +1,9 @@
+public class Distance {
+    public static void main(String[] args) {
+        double distance_in_km = 10.8;
+        double distance_in_meters = distance_in_km * 1000;
+        System.out.println("The distance 10.8 km in miles is: " + distance_in_meters);
+
+    }
+
+}
