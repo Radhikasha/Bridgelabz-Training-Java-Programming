096dcedb876837751d15d9925bbcdd4cf42c5e0b
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-09 12:27:44 +0530

    Added ques 2 in  Level-1 file

--- core.programming/core.programing.basic/programing.elements/level.1/CurrentYear.java
+++ core.programming/core.programing.basic/programing.elements/level.1/CurrentYear.java
@@ -3,7 +3,7 @@ public class CurrentYear {
         int harry_birth_year = 2000;
         int current_year = 2024;
         int harry_age = current_year - harry_birth_year;
-        System.out.println(harry_age);
+        System.out.println("Harry's age in 2024 is: " + harry_age);
 
     }
 }
