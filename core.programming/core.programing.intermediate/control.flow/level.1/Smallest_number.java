8d83a69867487fcbb1923a7811825c59d106e322
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-10 12:07:53 +0530

    added ques 2 in level 1

---
+++ core.programming/core.programing.intermediate/control.flow/level.1/Smallest_number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Smallest_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int smallest = num1;
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        System.out.println("The smallest number is: " + smallest);
+
+    }
+
+}
