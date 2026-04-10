fd2847e5105a438dafef086a0e1e4ebc9009871c
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-10 14:42:39 +0530

    added ques 8 in level 1

---
+++ core.programming/core.programing.intermediate/control.flow/level.1/count_while.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class count_while {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
+        }
+    }
+
+}
