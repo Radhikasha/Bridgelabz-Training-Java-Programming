734f7d7dfa594f458e2bcf5eb4dfffc1d1f54efe
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-10 10:42:36 +0530

    added ques 13 in level 1

---
+++ core.programming/core.programing.basic/programing.elements/level.1/Side_of_square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Side_of_square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int perimeter = sc.nextInt();
+        int side = perimeter / 4;
+        System.out.println("The side of square is: " + side);
+    }
+
+}
