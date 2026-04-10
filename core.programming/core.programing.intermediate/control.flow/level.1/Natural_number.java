a6c85bdf315d77aadf0fde1cba22752c81c90f61
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-10 14:11:50 +0530

    added ques 4 in level 1

---
+++ core.programming/core.programing.intermediate/control.flow/level.1/Natural_number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Natural_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0) {
+            int sum = number * (number + 1) / 2;
+
+            System.out.println("Sum of natural numbers up to " + number + " is: " + sum);
+        } else {
+            System.out.println(number + " is not a natural number.");
+        }
+    }
+
+}
