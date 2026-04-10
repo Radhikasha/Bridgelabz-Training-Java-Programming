bcfbe23878aad26394fa4c11ed1bec9c323baf6a
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-10 12:28:58 +0530

    added ques 3 in level 1

---
+++ core.programming/core.programing.intermediate/control.flow/level.1/Largest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int largest = num1;
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        System.out.println("The largest number is: " + largest);
+    }
+
+}
