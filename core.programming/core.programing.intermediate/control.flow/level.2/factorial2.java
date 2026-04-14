f8027da3e692ec97ffe60ac3003611652c9c92f7
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-14 14:41:46 +0530

    added ques 6

---
+++ core.programming/core.programing.intermediate/control.flow/level.2/factorial2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class factorial2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.println(i);
+                }
+                fact *= i;
+            }
+            System.out.println("Factorial of " + n + " is: " + fact);
+        } else {
+            System.out.println("Not a positive number");
+        }
+    }
+
+}
