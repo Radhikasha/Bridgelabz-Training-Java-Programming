4805356e2f2d1461dfd1405aa131c296b77066b7
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-04-15 12:18:59 +0530

    added ques 5

---
+++ core.programming/core.programing.intermediate/methods/Level.2/vote.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class vote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            age[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (canStudentVote(age[i])) {
+                System.out.println("Student " + (i + 1) + " can vote.");
+            } else {
+                System.out.println("Student " + (i + 1) + " cannot vote.");
+            }
+        }
+
+    }
+
+    public static boolean canStudentVote(int age) {
+        return age >= 18;
+    }
+
+}
