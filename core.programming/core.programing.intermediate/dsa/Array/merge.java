fedb8564313d6cb108542bb17d3e715f4436c6b7
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-07 16:47:27 +0530

    added merge array ques

---
+++ core.programming/core.programing.intermediate/dsa/Array/merge.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Arrays;
+
+public class merge {
+
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int i = 0; i < n; i++) {
+            nums1[m++] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void main(String[] args) {
+
+        int nums1[] = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+
+        int nums2[] = {2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+    }
+}
