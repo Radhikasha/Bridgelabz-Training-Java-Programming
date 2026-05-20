b80ac77bb913b86d1b8959bf9557ec9aebd20e5a
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-20 10:44:01 +0530

    added how to read a file

---
+++ core.programming/core.programing.intermediate/inputOutputStream/fileReader.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+public class fileReader {
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader fr = new FileReader("demo.txt");
+            int i;
+            while((i = fr.read()) != -1){
+                System.out.print((char)i);
+
+            }
+            fr.close();
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+    }
+    
+}
