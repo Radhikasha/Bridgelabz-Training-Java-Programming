534b5ac7bc2b2b7a4f8671c5db515fbc38c2dc18
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-26 16:26:43 +0530

    added an example of group count

---
+++ core.programming/core.programing.intermediate/regex/groupCount.java
@@ -0,0 +1,21 @@
+import java.util.regex.*;
+
+public class groupCount {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("(Geeks)");
+        Matcher m = p.matcher("GeeksForGeeks Geeks for For Geeks Geek");
+         System.out.println(m.groupCount());
+
+
+          Pattern p2 = Pattern.compile("(GFG)");
+        Matcher m2 = p2.matcher("FGF GF FG FGF");
+         System.out.println(m2.groupCount());
+
+
+         Pattern p3 = Pattern.compile("GFG");
+        Matcher m3 = p3.matcher("FGF GF FG FGF G F G");
+         System.out.println(m3.groupCount());
+
+    }
+    
+}
