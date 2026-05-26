6389763c661235e853183d78625dbfa2762ab47d
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-26 16:29:12 +0530

    some more concepts

--- core.programming/core.programing.intermediate/regex/groupCount.java
+++ core.programming/core.programing.intermediate/regex/groupCount.java
@@ -13,8 +13,14 @@ public class groupCount {
 
 
          Pattern p3 = Pattern.compile("GFG");
-        Matcher m3 = p3.matcher("FGF GF FG FGF G F G");
+        Matcher m3 = p3.matcher("FGF GF FG FGF G F G  GFG");
          System.out.println(m3.groupCount());
+
+
+          Pattern pattern = Pattern.compile("(a*b)(foo)(a)(d)");
+        Matcher matcher = pattern.matcher("aabfoo");
+
+        System.out.println("Groups in pattern: " + matcher.groupCount()); 
 
     }
     
