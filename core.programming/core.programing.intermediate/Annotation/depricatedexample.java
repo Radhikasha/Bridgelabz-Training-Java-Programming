4626118a69d13dddce7346611ccc086875f9381e
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-28 16:23:08 +0530

    added an example of depricated

---
+++ core.programming/core.programing.intermediate/Annotation/depricatedexample.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class depricatedexample{
+
+
+    @Deprecated
+    static  void display(){
+        System.out.println("hyy, i am radhika");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
