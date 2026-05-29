147fece5534281640c5967c3347d7566a52d7bba
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-05-29 11:11:05 +0530

    example of suppresswarning annotation

---
+++ core.programming/core.programing.intermediate/Annotation/suppresswarningsexample.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+public class suppresswarningsexample {
+
+     @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+       
+          ArrayList a = new ArrayList<>();
+          a.add("radhika");
+          
+
+          System.out.println(a);
+    }
+    
+}
