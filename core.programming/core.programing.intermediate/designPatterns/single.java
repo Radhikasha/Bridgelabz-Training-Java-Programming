99b8c057881d326c82817f640d78966a7aa532b9
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-06-02 12:39:06 +0530

    some changes

--- core.programming/core.programing.intermediate/designPatterns/single.java
+++ core.programming/core.programing.intermediate/designPatterns/single.java
@@ -1,3 +1,11 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+
 class eager{
     private static eager temp = new eager();
     public static  eager getEager(){
@@ -6,10 +14,15 @@ class eager{
 }
 
 
-class single{
+public class single implements Serializable{
 
   private static single temp;
-    private single(){
+    private single() {
+      /*   if(temp != null){
+            throw new RuntimeException("error");
+
+        }
+            */   //yeh reflection api se bchne k liye
 
     }
 // lazy way of creating singleton
@@ -32,7 +45,7 @@ class single{
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws Exception{
 
         // lazy way     dikkt yeh h ki method call hoga jb hi object create hoga
         single a = single.getSingle();
@@ -45,6 +58,27 @@ class single{
         //eager way  dikkt yeh h ki agr object use nhi krna phir bhi object create hoga
         System.out.println(eager.getEager().hashCode());
         System.out.println(eager.getEager().hashCode());
+
+
+        //1.  reflection api use to break singleton
+       Constructor<single> c = single.class.getDeclaredConstructor();
+        c.setAccessible(true); // bcz constr mera private h 
+        single s3 = c.newInstance();
+        System.out.println(s3.hashCode());
+        
+
+
+        // 2. deserialization
+        single ans = single.getSingle();
+        System.out.println(ans.hashCode());
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("abc.txt"));
+        oos.writeObject(ans);
+        System.out.println("SERIALIZATION done");
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("abc.txt"));
+        single ans2 = (single) ois.readObject();
+        System.out.println(ans2.hashCode());
+
     }
 
 }
