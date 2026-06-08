9683f323986d80539feca1ac16725fb1610470a9
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-06-08 11:31:56 +0530

    how to use functional interface

--- core.programming/core.programing.intermediate/lambda/anonymousClass.java
+++ core.programming/core.programing.intermediate/lambda/anonymousClass.java
@@ -1,12 +1,24 @@
 public interface car{
     public abstract void print();
 }
+class exa implements car{
+    public void print(){
+        System.out.println("using class");
+
+    }
+}
 
 
 
 public class anonymousClass {
 
     public static void main(String[] args) {
+
+        // using normal method
+     exa e = new exa();
+     e.print();
+
+        // anonymous class 
         car c = new car(){
             public void print(){
                 System.out.println("Anonymous class");
@@ -23,6 +35,14 @@ public class anonymousClass {
             }
         };
         c2.print();
+
+// using lambda 
+      car c3 = ()->{
+        System.out.println("This is using lambda ");
+      };
+      c3.print();
+
+
     }
     
 }
