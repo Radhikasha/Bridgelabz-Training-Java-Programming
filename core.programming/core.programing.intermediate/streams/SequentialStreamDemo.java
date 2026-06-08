2cabf086c10391c5f94136d2f80e4d2620b32a69
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-06-08 16:41:23 +0530

    example of sequential stream

---
+++ core.programming/core.programing.intermediate/streams/SequentialStreamDemo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class SequentialStreamDemo {
+
+     public static void main(String[] args) {
+        List<String> names = Arrays.asList("A", "B", "C", "D");
+
+        names.stream()
+             .forEach(System.out::println); // Executes sequentially
+    }
+    
+}
