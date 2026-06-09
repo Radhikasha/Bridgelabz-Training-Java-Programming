b5780d68fab47ad9bca6aa1b6963eb51d88ad223
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-06-09 12:29:35 +0530

    thread using runnable interface

--- core.programming/core.programing.intermediate/thread/usingRunnable.java
+++ core.programming/core.programing.intermediate/thread/usingRunnable.java
@@ -2,6 +2,11 @@ public class usingRunnable  implements Runnable{
     public void run(){
         for(int i=0;i<=10;i++){
             System.out.println(i);
+            try {
+                Thread.sleep(1000);
+                
+            } catch (Exception e) {
+            }
         }
 
     }
