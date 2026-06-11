4ed3592c8cda999b7245a32b4660d3e0f440c84a
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2026-06-11 20:06:29 +0530

    delete extra

--- core.programming/core.programing.basic/programing.elements/level.1/CurrentYear.java
+++
@@ -1,9 +0,0 @@
-public class CurrentYear {
-    public static void main(String[] args) {
-        int harry_birth_year = 2000;
-        int current_year = 2024;
-        int harry_age = current_year - harry_birth_year;
-        System.out.println("Harry's age in 2024 is: " + harry_age);
-
-    }
-}
